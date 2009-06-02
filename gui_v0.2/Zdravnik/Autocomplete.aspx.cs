fe28b3be8a6f0a2bcdd1e05025d257710720171b
krizko <krizko@d90ba54e-3ecc-11de-9b50-8bb2048eb44c>
2009-06-02 17:55:26 +0000

    Autocomplete.aspx in Autocomplete.aspx.cs dodan. Izvor za autocompletion v JS

---
+++ gui_v0.2/Zdravnik/Autocomplete.aspx.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Xml.Linq;
+
+public partial class Zdravnik_Autocomplete : System.Web.UI.Page
+{
+	protected const string EOL = "\n";
+	
+	protected void Page_Load(object sender, EventArgs e)
+	{
+		string QString = Request.QueryString["q"];
+		string Type    = Request.QueryString["type"];
+		
+		// ne se mantrat, če ni zadost 
+		if(QString == null || QString.Length < 1)
+		return;
+		
+		if(Type == null)
+		return;
+		
+		// činkeni
+		Type = Type.ToLower();
+		
+		// vrnemo tu sem
+		string Ret = "";
+		
+		// križišče
+		if(Type == "pacient")
+		{
+			IPNMP.Pacient[] Pacienti = IPNMP.Pacient.VrniVse();
+			
+			foreach(IPNMP.Pacient P in Pacienti)
+			{
+				string[] s =
+				{
+					P.Ime,
+					P.Priimek,
+					P.EMŠO,
+					P.Naslov.Mesto,
+					P.Naslov.Ulica,
+					P.Naslov.PoštnaŠtevilka.ToString()
+				};
+				
+				Ret += Delno(s, QString);
+			}
+		}
+		else if(Type == "zdravnik")
+		{
+			// ni ga!
+		}
+		
+		//
+		// Ok, pošlji kaj mamo
+		Response.Write(Ret);
+	}
+	
+	protected bool Delno(string Tekst, string Najdi)
+	{
+		// Zamenjaj z IndexOf() da najde bilokje v tekstu
+		if(Tekst.StartsWith(Najdi, StringComparison.CurrentCultureIgnoreCase))
+		{
+			return true;
+		}
+		
+		return false;
+	}
+	
+	protected string Delno(string[] Tekst, string Najdi)
+	{
+		string TekstUjemanje = "";
+		
+		foreach(string s in Tekst)
+		{
+			if(Delno(s, Najdi))
+			{
+				TekstUjemanje += s;
+				TekstUjemanje += EOL;
+			}
+		}
+		
+		return TekstUjemanje;
+	}
+}
